79b4b06848ce479cd54c9d9286648d3ce7723b7d
luckydwipasb@students.amikom.ac.id <luckydwipasb@gmail.com>
2022-03-24 12:57:30 +0700

    inisialisasi project3

---
+++ latihanclassdanobject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclassdanobject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai propertis
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.Numpassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ latihanclassdanobject/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclassdanobject
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int Numpassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number Of Passenger : {0}\n", Numpassenger);
+        }
+        public void PickupPassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+
+    }
+}
